fd4c91d86c97bf6120af523a4f55382558b564c3
Michal <mt.michaltangri@gmail.com>
2020-10-13 14:14:32 +0200

    Add sample OpenCV program

---
+++ Sample/main.cpp
@@ -0,0 +1,9 @@
+#include <opencv.hpp>
+#include <highgui/highgui_c.h>
+#include <iostream>
+int main( int argc, char** argv )
+{
+    cv::namedWindow("Okienko Prawie Inteligentne", 1);
+    cv::waitKey(0);
+    return 0;
+}

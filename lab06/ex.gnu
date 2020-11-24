fe7ed974dac0b325e7326333f1704f867557de5e
Michal <mt.michaltangri@gmail.com>
2020-11-24 12:09:22 +0100

    Add chart generation for lab06

---
+++ lab06/ex.gnu
@@ -0,0 +1,4 @@
+set style data lines
+set xlabel 'iterations'
+set ylabel 'result'
+plot 'data.txt'
--- lab06/main.cpp
+++ lab06/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <functional>
+#include <fstream>
 
 
 using namespace std;
 random_device r;
 default_random_engine e1(r());
+
+void saveDataToFile(vector<double> data) {
+    ofstream output;
+    output.open("data.txt");
+    if (!output)
+        cerr << "Cannot open 'data.txt' file!" << endl;
+    for (int i = 0; i < data.size(); i++) {
+        output << (i + 1) * 10000 << "   " << data.at(i) << "\n";
+    }
+    output.close();
+}
 
 auto generate_values = [](auto randomizer) {
     vector<double> tmp(2);
@@ -14,7 +27,7 @@ auto generate_values = [](auto randomizer) {
     return tmp;
 };
 
-auto optimalization_algorithm = [](auto func, auto domain, int max_iterations) {
+auto optimization_algorithm = [](auto func, auto domain, int max_iterations) {
     uniform_real_distribution<double> uniform_dist(domain[0], domain[1]);
     auto best = generate_values(uniform_dist);
     for (int i = 0; i < max_iterations; i++) {
@@ -24,6 +37,21 @@ auto optimalization_algorithm = [](auto func, auto domain, int max_iterations) {
         }
     }
     return best;
+};
+
+auto optimization_algorithm_gnuplot = [](auto func, auto domain, int max_iterations) {
+    uniform_real_distribution<double> uniform_dist(domain[0], domain[1]);
+    vector<double> tmp;
+    auto best = generate_values(uniform_dist);
+    for (int i = 1; i <= max_iterations; i++) {
+        auto solution = generate_values(uniform_dist);
+        if (func(best) > func(solution)) {
+            best = solution;
+        }
+        if (i % 10000 == 0)
+            tmp.push_back(func(best));
+    }
+    return tmp;
 };
 
 int main()
@@ -67,17 +95,17 @@ int main()
         return sum;
     };
 
-    std::vector<double> camel_domain {-5, 5};
-    std::vector<double> booth_domain {-10, 10};
-    std::vector<double> beale_domain {-4.5, 4.5};
-    std::vector<double> sphere_domain {-100, 100};
+    vector<double> camel_domain {-5, 5};
+    vector<double> booth_domain {-10, 10};
+    vector<double> beale_domain {-4.5, 4.5};
+    vector<double> sphere_domain {-100, 100};
 
-    int iterations = 100000;
+    int iterations = 1000000;
 
-    auto solution_camel = optimalization_algorithm(three_hump_camel, camel_domain, iterations);
-    auto solution_booth = optimalization_algorithm(booth_function, booth_domain, iterations);
-    auto solution_beale = optimalization_algorithm(beale_function, beale_domain, iterations);
-    auto solution_sphere = optimalization_algorithm(sphere_function, sphere_domain, iterations);
+    auto solution_camel = optimization_algorithm(three_hump_camel, camel_domain, iterations);
+    auto solution_booth = optimization_algorithm(booth_function, booth_domain, iterations);
+    auto solution_beale = optimization_algorithm(beale_function, beale_domain, iterations);
+    auto solution_sphere = optimization_algorithm(sphere_function, sphere_domain, iterations);
 
     cout << "Camel: [ ";
     for (auto e : solution_camel)
@@ -98,4 +126,24 @@ int main()
     for (auto e : solution_sphere)
         cout << e << " ";
     cout << "] -> " << sphere_function(solution_sphere) << endl;
+
+    vector<vector<double>> gnuplot_data;
+    for (int i = 0; i < 20; i++) {
+        gnuplot_data.push_back(optimization_algorithm_gnuplot(booth_function, booth_domain, iterations));
+    }
+
+    const unsigned int results_size = gnuplot_data.at(0).size();
+
+    vector<double> averages_from_data;
+    for (int i = 0; i < results_size; i++) {
+        double sum = 0;
+        for (int j = 0; j < gnuplot_data.size(); j++) {
+            sum += gnuplot_data.at(j).at(i);
+        }
+        double average = sum / results_size;
+        averages_from_data.push_back(average);
+    }
+
+    saveDataToFile(averages_from_data);
+    
 }
